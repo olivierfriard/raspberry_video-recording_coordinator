0ad4b6991953d55bcbb7faad11a4cfc5ee01ab65
Olivier Friard <olivier.friard@unito.it>
2021-09-17 10:53:16 +0200

    some work

--- src/worker/config.py
+++ src/worker/config.py
@@ -10,13 +10,13 @@ PORT = 5000
 DEFAULT_PICTURE_WIDTH = 640
 DEFAULT_PICTURE_HEIGHT = 480
 
-VIDEO_ARCHIVE = "/home/pi/video/static/video_archive"
+VIDEO_ARCHIVE = "/home/pi/worker/static/video_archive"
 DEFAULT_VIDEO_DURATION = 10 # seconds
 DEFAULT_FPS = 10
 DEFAULT_VIDEO_WIDTH = 640
 DEFAULT_VIDEO_HEIGHT = 480
 DEFAULT_VIDEO_QUALITY = 1 # Mb/s
 
-LOG_PATH = "/home/pi/video/worker.log"
+LOG_PATH = "/home/pi/worker/worker.log"
 
 WIFI_INTERFACE = "wlan0"
