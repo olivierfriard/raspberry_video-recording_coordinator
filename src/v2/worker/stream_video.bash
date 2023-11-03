eb4e4d7d1cd9c921e247fc5bec6b15811c3ca958
Olivier Friard <olivier.friard@unito.it>
2023-11-03 17:18:13 +0100

    some work

--- src/v2/worker/stream_video.bash
+++ src/v2/worker/stream_video.bash
@@ -1,5 +1,6 @@
 #/bin/bash
 
-/usr/bin/libcamera-vid -t 0 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264
+# /usr/bin/libcamera-vid -t 0 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264
 
 
+/usr/bin/libcamera-vid -t 0 --inline --listen -o tcp://0.0.0.0:6000
